0a4179be09194c383f5249b5ab101c625fc53b5b
romankt21 <romankt21@gmail.com>
2022-04-29 20:57:21 +0300

    lec6_part_2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
-const App() => {
+import {useDispatch, useSelector} from "react-redux";
+
+const App = () => {
+    let state = useSelector(state => state);
+    let dispatch = useDispatch();
   return (
-      <div>Hello world</div>
+      <div>
+
+          <h1>state is - {state}</h1>
+        <button onClick={() => {
+            dispatch({type: 'INC', payload: 1})
+        }} > increment
+        </button>
+
+        <button onClick={() =>{
+            dispatch({type: 'DEC', payload: 1})
+        }}>decrement
+        </button>
+
+
+          <button onClick={() =>{
+              dispatch({type: 'RESET', payload: 0})
+          }}>reset
+          </button>
+      </div>
+
+
   );
 }
 
